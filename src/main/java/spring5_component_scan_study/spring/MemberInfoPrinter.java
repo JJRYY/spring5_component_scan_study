5be13d583daf20c6f22758c0e623156071f6bc39
Ryong <kjr2436@gmail.com>
2021-05-06 16:11:30 +0900

    spring5_component_scan

---
+++ src/main/java/spring5_component_scan_study/spring/MemberInfoPrinter.java
@@ -0,0 +1,25 @@
+package spring5_component_scan_study.spring;
+
+public class MemberInfoPrinter {
+	private MemberDao memDao;
+	private MemberPrinter printer;
+
+	public void MemberInfoPrinter(String email) {
+		Member member = memDao.selectByEmail(email);
+		if (member == null) {
+			System.out.println("데이터 없음\n");
+			return;
+		}
+		printer.print(member);
+		System.out.println();
+	}
+
+	public void setMemDao(MemberDao memDao) {
+		this.memDao = memDao;
+	}
+
+	public void setPrinter(MemberPrinter printer) {
+		this.printer = printer;
+	}
+
+}
---
+++ src/main/java/spring5_component_scan_study/spring/MemberSelectAllService.java
@@ -0,0 +1,24 @@
+package spring5_component_scan_study.spring;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class MemberSelectAllService {
+//	private MemberDao memberDao;
+//
+//	public MemberSelectAllService(MemberDao memberDao) {
+//		this.memberDao = memberDao;
+//	}
+//	
+//	public void selectAll(){
+//		Map<String, Member> map = memberDao.selectAll();
+//		
+//		Set<Entry<String,Member>> entrySet = map.entrySet();
+//		
+//		for(Entry<String, Member> entry : entrySet) {
+//			System.out.printf("키:%s, 값:%s%n", entry.getKey(), entry.getValue());
+//		}
+//	}
+	
+}
